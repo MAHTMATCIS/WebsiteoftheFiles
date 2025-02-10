0a1c2b4eddc7c4c4234d6043d11ffbecbb31ba8f
MAHTMATCIS <tongmx33@163.com>
2025-02-10 16:04:48 +0800

    Final v0.0.1-05

--- js/mainIndex.js
+++ js/mainIndex.js
@@ -3,22 +3,25 @@ function leftMenu(){
     let leftMenu2 = document.getElementById("left-menu");
     let items1= document.getElementsByClassName("menu-btn");
     let items2= document.getElementsByClassName("menu-div");
-    console.log(leftMenu.style.left)
+    console.debug(leftMenu.style.left)
     if (leftMenu.style.left==="0px"){
         leftMenu.style.left="300px";
         leftMenu.src="img/left_arrow.png"
         leftMenu2.style.width="300px"
-        for (let i in items1){
-            items1[i].style.opacity=1;
-            items2[i].style.opacity=1;
+        for (const i of items1){
+            i.style.opacity=1;
+        }
+        for (const i of items2){
+            i.style.opacity=1;
         }
     }else{
         leftMenu.style.left="0";
         leftMenu.src="img/right_arrow.png"
         leftMenu2.style.width="0"
-        for (let i in items1){
-            items1[i].style.opacity=0;
-            items2[i].style.opacity=0;
+        for (const i of items1){
+            i.style.opacity=0;
+        }for (const i of items2){
+            i.style.opacity=0;
         }
     }
 }
